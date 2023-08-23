927dffb02538099b9f861ac29c5c3882b3153f95
Freddy Soto <freddy.sotof23@hotmail.com>
2023-08-23 00:28:17 -0400

    feat: front-end implementation

--- FrontEnd/src/store/store.js
+++ FrontEnd/src/store/store.js
@@ -4,7 +4,6 @@ import { productSlice } from "./product";
 import { authSlice } from "./auth";
 import { uiSlice } from "./ui";
 import { orderSlice } from "./order";
-import { userSlice } from "./user";
 
 
 export const store = configureStore({
@@ -13,7 +12,6 @@ export const store = configureStore({
         cart: cartSlice.reducer,
         order:orderSlice.reducer,
         product:productSlice.reducer,
-        user:userSlice.reducer,
         ui: uiSlice.reducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
